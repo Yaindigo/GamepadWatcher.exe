afd85b995d97371907dd00863fa46e0d2124c893
Dmitriy <44805013+Yaindigo@users.noreply.github.com>
2025-07-05 18:40:39 +0300

    Add files via upload

---
+++ GamePadBP.cpp
@@ -0,0 +1,65 @@
+
+#include <Windows.h>
+#include <Xinput.h>
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+#pragma comment(lib, "Xinput.lib")
+
+void SimulateAltF4() {
+    INPUT input[4] = {};
+
+    // Нажатие Alt
+    input[0].type = INPUT_KEYBOARD;
+    input[0].ki.wVk = VK_MENU; // Alt
+
+    // Нажатие F4
+    input[1].type = INPUT_KEYBOARD;
+    input[1].ki.wVk = VK_F4;
+
+    // Отпускание F4
+    input[2].type = INPUT_KEYBOARD;
+    input[2].ki.wVk = VK_F4;
+    input[2].ki.dwFlags = KEYEVENTF_KEYUP;
+
+    // Отпускание Alt
+    input[3].type = INPUT_KEYBOARD;
+    input[3].ki.wVk = VK_MENU;
+    input[3].ki.dwFlags = KEYEVENTF_KEYUP;
+
+    SendInput(4, input, sizeof(INPUT));
+    std::cout << "[INFO] Alt+F4 simulated." << std::endl;
+}
+
+bool IsControllerConnected() {
+    XINPUT_STATE state;
+    ZeroMemory(&state, sizeof(XINPUT_STATE));
+    return (XInputGetState(0, &state) == ERROR_SUCCESS);
+}
+
+int main() {
+    std::cout << "=== Gamepad Watcher Started ===" << std::endl;
+
+    bool wasConnected = IsControllerConnected();
+    if (wasConnected) {
+        std::cout << "[INFO] Gamepad initially connected." << std::endl;
+    }
+    else {
+        std::cout << "[INFO] Gamepad initially NOT connected." << std::endl;
+    }
+    ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
+    while (true) {
+        bool connected = IsControllerConnected();
+
+        if (!connected && wasConnected) {
+            std::cout << "[EVENT] Gamepad disconnected!" << std::endl;
+            SimulateAltF4();
+        }
+
+        wasConnected = connected;
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+    }
+
+    return 0;
+}
